b11d479476003efe1aad1283e91baae85cbc6deb
Naveen B Krishnamohan <nbk@teksystems.com>
2019-12-06 05:05:24 -0500

    initial

---
+++ ResumeRevealer/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Http;
+
+namespace ResumeRevealer.Controllers
+{
+    public class HomeController : ApiController
+    {
+        //        [BasicAuthentication(ServiceName = "RestAPI")]
+        [HttpGet]
+        [Route("test")]
+        public IHttpActionResult HasAccesstoModules()
+        {
+            var result = "Test message !!";
+            
+            return this.Ok(result);
+        }
+    }
+}
---
+++ ResumeRevealer/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ResumeRevealer.WebApiApplication" Language="C#" %>
