2cfda8bf2296d27a20dbab978226b7cba9f61a69
vbelle <vbelle@BSC-PG01M1T4>
2019-12-07 07:28:17 -0500

    added optimization for skill set

--- Sharpenter.ResumeParser.ResumeProcessor/Parsers/SkillsParser.cs
+++ Sharpenter.ResumeParser.ResumeProcessor/Parsers/SkillsParser.cs
@@ -1,13 +1,34 @@
 ﻿using ResumeParser.Model;
 using ResumeParser.Model.Models;
+using ResumeParser.ResumeProcessor.Helpers;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 
 namespace ResumeParser.ResumeProcessor.Parsers
 {
     public class SkillsParser : IParser
-    {        
+    {
+        private readonly List<string> _technologylist;
+        public SkillsParser() {
+         
+            try
+            {   // Open the text file using a stream reader.
+                using (StreamReader sr = new StreamReader("C:\\data\\Technology.txt"))
+                {
+                    // Read the stream to a string, and write the string to the console.
+                    String line = sr.ReadToEnd();
+                    _technologylist = line.Split(',').ToList();
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+        }
         public void Parse(Section section, Resume resume)
         {
             if (resume.Skills == null)
@@ -38,6 +59,19 @@ namespace ResumeParser.ResumeProcessor.Parsers
                     resume.Skills.AddRange(skills.Select(e => e.Trim()));
                 }
                 resume.Skills= resume.Skills.Where(w => w.Any(c => !Char.IsDigit(c))).ToList();
+                List<string> optimisedList = new List<string>();
+                foreach(var skills in resume.Skills)
+                {
+                    foreach(var tech in _technologylist)
+                    {
+                        if (skills.ToLower().Contains(tech.ToLower()))
+                        {
+                            optimisedList.Add(skills);
+                            break;
+                        }
+                    }
+                }
+                resume.Skills = optimisedList;
             }
         }
     }
